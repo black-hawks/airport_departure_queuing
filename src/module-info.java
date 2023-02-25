c651a64142c75512ace1279c723659441d94ab35
Purvish Shah <purvish14@gmail.com>
2023-02-25 10:56:57 +0000

    Update metadata

--- src/module-info.java
+++ src/module-info.java
@@ -5,8 +5,5 @@
  * @author purvi
  */
 module airport_departure_queuing {
-    requires simpleclient;
-    requires simpleclient.httpserver;
-    requires simpleclient.common;
     requires slf4j.api;
 }
